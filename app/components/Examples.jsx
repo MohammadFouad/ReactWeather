81cd3a79e4e1d185dc833d447250a01543b282c3
root <manomano00@outlook.com>
2017-03-02 16:28:40 +0200

    para

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,14 +1,21 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
-    return (
-
-        <div>
-            <h1>Examples Component,,,,,</h1>
-            <h3>Examples Componentar</h3>
-
-        </div>
-    )
+  return (
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Philadelphia'>Philadelphia, PA</Link>
+        </li>
+        <li>
+          <Link to='/?location=Rio'>Rio, Brazil</Link>
+        </li>
+      </ol>
+    </div>
+  )
 };
 
 module.exports = Examples;
